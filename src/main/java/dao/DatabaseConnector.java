bff862e884d2500b560c612c6446ec14917a94a3
Jakub Adamczyk <jakubadamczyk10@gmail.com>
2019-11-30 17:09:58 +0100

    Added DatabaseConnector and tests

---
+++ src/main/java/dao/DatabaseConnector.java
@@ -0,0 +1,46 @@
+package dao;
+
+import com.mongodb.ConnectionString;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
+
+/**
+ * Singleton class existing to encapsulate access to the only existing database instance.
+ */
+public class DatabaseConnector
+{
+    private static final DatabaseConnector DB = new DatabaseConnector();
+
+    private MongoClient mainMongoClient = MongoClients.create(
+            "mongodb+srv://mainuser:MainUser10@maincluster-af4rk.gcp.mongodb.net/test?retryWrites=true&w=majority");
+    private MongoClient testMongoClient = MongoClients.create(
+            "mongodb+srv://mainuser:MainUser10@testcluster-5qtaz.gcp.mongodb.net/test?retryWrites=true&w=majority");
+
+    private MongoDatabase mainDatabase;
+    private MongoDatabase testDatabase;
+
+    private DatabaseConnector() {
+    }
+
+    /**
+     * Return instance of appropriate database.
+     *
+     * @param mainCluster if true, return main database instance; else return test database instance
+     * @return database instance
+     */
+    public static MongoDatabase getDB(boolean mainCluster) {
+        if (mainCluster) {
+            if (DB.mainDatabase == null)
+                DB.mainDatabase = DB.mainMongoClient.getDatabase("mainDB");
+
+            return DB.mainDatabase;
+        }
+        else {
+            if (DB.testDatabase == null)
+                DB.testDatabase = DB.mainMongoClient.getDatabase("testDB");
+
+            return DB.testDatabase;
+        }
+    }
+}
---
+++ src/test/java/dao/DatabaseConnectorTests.java
@@ -0,0 +1,34 @@
+package dao;
+
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class DatabaseConnectorTests
+{
+    MongoDatabase db;
+
+    @BeforeClass
+    public static void testConnection()
+    {
+        MongoDatabase db = DatabaseConnector.getDB(false);
+        // will throw an exception if connection could not be made (= db is null)
+        db.getName();
+    }
+
+    @Before
+    public void setupDatabase()
+    {
+        this.db = DatabaseConnector.getDB(false);
+        // clear all collections with empty Document filter
+        for (String collectionName: db.listCollectionNames())
+            db.getCollection(collectionName).deleteMany(new Document());
+    }
+
+    @Test
+    public void tmp()
+    {
+    }
+}
