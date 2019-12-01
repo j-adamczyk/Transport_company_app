407eff787d31e0a58995bac3e064e305b8e0e13e
Natalia Organek <natwlo6@wp.pl>
2019-12-01 16:45:00 +0100

    companyTests

--- src/test/java/dao/CompanyDAOTests.java
+++ src/test/java/dao/CompanyDAOTests.java
@@ -9,20 +9,25 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import java.util.List;
+
+import static org.junit.Assert.*;
+
 public class CompanyDAOTests {
 
     MongoDatabase db;
     Company c1;
     CompanyDAO companyDAO;
+    Company c2;
 
     @BeforeClass
-    public void testConnection()
+    public static void testConnection()
     {
         DbConnector.getInstance().setDbType(false);
         MongoDatabase db = DbConnector.getDB();
         // will throw an exception if connection could not be made (= db is null)
         db.getName();
-        this.db = DbConnector.getDB();
+        db = DbConnector.getDB();
         for (String collectionName: db.listCollectionNames())
             db.getCollection(collectionName).deleteMany(new Document());
     }
@@ -30,10 +35,14 @@ public class CompanyDAOTests {
     @Before
     public void setupDatabase()
     {
+        this.db = DbConnector.getDB();
         c1 = new Company("Company1", new Address("Poland", "Krakow", "33-333", "Krakowska 17"),
                 "444555666", "xxx@xxx.pl", "Adam Kowalski");
+        c2 = new Company("Company2", new Address("Germany", "Berlin", "G33-333", "Gute Strasse 88"),
+                "123123123", "www@xxx.pl", "Bernard Krausse");
         companyDAO = new CompanyDAO();
         companyDAO.save(c1);
+        companyDAO.save(c2);
 
     }
 
@@ -43,9 +52,27 @@ public class CompanyDAOTests {
             db.getCollection(collectionName).deleteMany(new Document());
     }
 
+
     @Test
-    public void tmp()
+    public void companyDeleteTest()
     {
+        companyDAO.delete(c1.get_id());
+        List<Company> allCompanies = companyDAO.findAllCompanies();
+        assertEquals(allCompanies.size(), 1);
+        assertTrue(allCompanies.contains(c2));
+        assertFalse(allCompanies.contains(c1));
+    }
+
+    @Test
+    public void companyFindTest(){
+        Company found = companyDAO.find(c1.get_id());
+        assertEquals(found, c1);
+    }
+
+    @Test
+    public void companyUpdateTest(){
+        Company changedC1 = new Company("Company1", new Address("Poland", "Krakow", "33-333", "Krakowska 17"),
+                "444555666", "xxx@xxx.pl", "Adam Kowalski")
     }
 
 
