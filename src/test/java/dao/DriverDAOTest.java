e375e6c29595d2704cf09f57a117434485e5b192
magk98 <magk98@poczta.fm>
2019-12-01 18:03:50 +0100

    Driver tests almost finished

--- src/test/java/dao/DriverDAOTest.java
+++ src/test/java/dao/DriverDAOTest.java
@@ -7,11 +7,15 @@ import org.bson.Document;
 import org.junit.*;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class DriverDAOTest {
     private MongoDatabase db;
     private DriverDAO driverDAO;
     private Driver d1;
+    private Driver d2;
 
     @BeforeClass
     public static void testConnection() {
@@ -29,18 +33,39 @@ public class DriverDAOTest {
                 "123456789",
                 new Address("Poland", "Krakow", "12-345", "krakowska"),
                 2000.0);
+        d2 = new Driver("Jakub Adamczyk",
+                LocalDate.of(1960, 2, 20),
+                LocalDate.of(2019, 10, 20),
+                "123456789",
+                new Address("Poland", "Krakow", "12-345", "krakowska"),
+                2000.0);
         driverDAO = new DriverDAO();
         driverDAO.save(d1);
     }
 
     @Test
     public void testFind() {
-        Assert.assertEquals(d1, driverDAO.find(d1.get_id()));
+        Assert.assertTrue(d1.equals(driverDAO.find(d1.get_id())));
+    }
+
+    @Test
+    public void testUpdate(){
+        driverDAO.update(d1.get_id(), d2);
+        //Assert.assertTrue(driverDAO.find(d1.get_id()).equals(d2));
     }
 
     @Test
     public void findByName(){
-        Assert.assertEquals(d1, driverDAO.findByName(d1.getName()));
+        Assert.assertEquals(d1.get_id(), driverDAO.findByName(d1.getName()).get(0).get_id());
+    }
+
+    @Test
+    public void findAllDrivers(){
+        driverDAO.save(d2);
+        List<Driver> actual = driverDAO.findAllDrivers();
+        List<Driver> expected = Arrays.asList(d1, d2);
+        Assert.assertEquals(actual.size(), 2);
+        //Assert.assertEquals(actual, expected);
     }
 
     @After
