a49fd5a53ee43c99a5de26d443e3ba5ab4c53a23
Natalia Organek <natwlo6@wp.pl>
2019-12-13 13:47:37 +0100

    Added javafx and packages view and presenter

--- src/main/java/TransportCompanyApp.java
+++ src/main/java/TransportCompanyApp.java
@@ -1,4 +1,21 @@
-public class TransportCompanyApp {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class TransportCompanyApp extends Application {
+
+    private Stage primaryStage;
+
+    @Override
+    public void start(Stage primaryStage) {
+
+        this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("My first JavaFX app");
+        this.primaryStage.show();
+
+    }
+
     public static void main(String[] args) {
+        launch(args);
     }
 }
+
