15243ee82cb1049d7798b6bd3a847d4917c5c804
Natalia Organek <natwlo6@wp.pl>
2019-12-15 15:25:03 +0100

    AddCompanyView and Presenter

---
+++ src/main/java/app/presenter/AddCompanyViewPresenter.java
@@ -0,0 +1,33 @@
+package app.presenter;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+
+public class AddCompanyViewPresenter {
+
+    @FXML
+    private TextField nameField;
+    @FXML
+    private TextField addressField;
+    @FXML
+    private TextField phoneField;
+    @FXML
+    private TextField mailField;
+    @FXML
+    private TextField representativeField;
+    @FXML
+    private Button acceptButton;
+    @FXML
+    private Button cancelButton;
+
+    @FXML
+    private void handleAcceptButtonAction(){
+//        TODO
+    }
+    @FXML
+    private void handleCancelButtonAction(){
+//        TODO
+    }
+}
