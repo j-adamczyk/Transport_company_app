c3091d029b727c5b905c14309c25d6bba5337531
Natalia Organek <natwlo6@wp.pl>
2019-12-15 13:55:27 +0100

    Added DriversView and DriversViewPresenter fixed tables in SelectedCurrentTransactionView

---
+++ src/main/java/app/presenter/DriversViewPresenter.java
@@ -0,0 +1,43 @@
+package app.presenter;
+
+import app.model.Driver;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+public class DriversViewPresenter {
+    @FXML
+    private TableView<Driver> driverTableView;
+    @FXML
+    private TableColumn<Driver, String> driverName;
+    @FXML
+    private TableColumn<Driver, String> driverBirthDay;
+    @FXML
+    private TableColumn<Driver, String> driverHireDate;
+    @FXML
+    private TableColumn<Driver, String> driverPhone;
+    @FXML
+    private TableColumn<Driver, String> driverAddress;
+    @FXML
+    private TableColumn<Driver, String> driverSalary;
+    @FXML
+    private Button addButton;
+    @FXML
+    private Button deleteButton;
+    @FXML
+    private Button editButton;
+
+    @FXML
+    private void handleAddButtonAction(){
+//        TODO
+    }
+    @FXML
+    private void handleDeleteButtonAction(){
+//        TODO
+    }
+    @FXML
+    private void handleEditButtonAction(){
+//        TODO
+    }
+}
