dec8c60ec3f431c26e136ff28028729b5e3cfcba
Natalia Organek <natwlo6@wp.pl>
2019-12-15 14:18:23 +0100

    AddDriverView and AddDriverViewPresenter

---
+++ src/main/java/app/presenter/AddDriverViewPresenter.java
@@ -0,0 +1,36 @@
+package app.presenter;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+
+
+public class AddDriverViewPresenter {
+    @FXML
+    private TextField nameField;
+    @FXML
+    private TextField addressField;
+    @FXML
+    private TextField phoneField;
+    @FXML
+    private TextField salaryField;
+    @FXML
+    private DatePicker birthDatePicker;
+    @FXML
+    private DatePicker hireDatePicker;
+    @FXML
+    private Button acceptButton;
+    @FXML
+    private Button cancelButton;
+
+    @FXML
+    private void handleAcceptButtonAction(){
+//        TODO
+    }
+    @FXML
+    private void handleCancelButtonAction(){
+//        TODO
+    }
+
+}
