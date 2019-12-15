570ceefd56745ab616fbba80123b24312ec0377f
Natalia Organek <natwlo6@wp.pl>
2019-12-15 16:29:25 +0100

    Add VehicleView and Presenter

---
+++ src/main/java/app/presenter/AddVehicleViewPresenter.java
@@ -0,0 +1,33 @@
+package app.presenter;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+
+public class AddVehicleViewPresenter {
+
+    @FXML
+    private TextField modelField;
+    @FXML
+    private TextField registrationField;
+    @FXML
+    private DatePicker manufactureDatePicker;
+    @FXML
+    private TextField cargoVolumeField;
+    @FXML
+    private TextField cargoWeightField;
+    @FXML
+    private Button acceptButton;
+    @FXML
+    private Button cancelButton;
+
+    @FXML
+    private void handleAcceptButtonAction(){
+//        TODO
+    }
+    @FXML
+    private void handleCancelButtonAction(){
+//        TODO
+    }
+}
