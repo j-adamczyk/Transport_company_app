c73dc84365a9310fbf253f98a73fb256df45d4cd
Natalia Organek <natwlo6@wp.pl>
2019-12-15 13:31:47 +0100

    AddTransport View and Presenter

---
+++ src/main/java/app/presenter/AddTransportPresenter.java
@@ -0,0 +1,67 @@
+package app.presenter;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+
+public class AddTransportPresenter {
+
+    @FXML
+    private ChoiceBox<String> currentTransactionIdChoiceBox;
+    @FXML
+    private Label viewCurrentTransactionsLabel;
+    @FXML
+    private ChoiceBox<String> driverChoiceBox;
+    @FXML
+    private Label viewDriversLabel;
+    @FXML
+    private DatePicker datePicker;
+    @FXML
+    private ChoiceBox<String> vehicleChoiceBox;
+    @FXML
+    private Label viewVehiclesLabel;
+    @FXML
+    private Button acceptButton;
+    @FXML
+    private Button cancelButton;
+
+
+    @FXML
+    private void handleCurrTransChoiceBoxAction(){
+//        TODO
+    }
+    @FXML
+    private void handleViewCurrTransLabelAction(){
+//        TODO
+    }
+    @FXML
+    private void handleDriverChoiceBoxAction(){
+//        TODO
+    }
+    @FXML
+    private void handleViewDriversLabelAction(){
+//        TODO
+    }
+    @FXML
+    private void handleDatePickerAction(){
+//        TODO
+    }
+    @FXML
+    private void handleVehicleChoiceBoxAction(){
+//        TODO
+    }
+    @FXML
+    private void handleViewVehiclesLabelAction(){
+//        TODO
+    }
+    @FXML
+    private void handleAcceptButtonAction(){
+//        TODO
+    }
+    @FXML
+    private void handleCancelButtonAction(){
+//        TODO
+    }
+}
