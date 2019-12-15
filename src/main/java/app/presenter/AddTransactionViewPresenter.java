889490cc743506b699370f36bbf5b4f5d8edba05
Natalia Organek <natwlo6@wp.pl>
2019-12-15 17:39:03 +0100

    Added AddTransactionView and Presenter

---
+++ src/main/java/app/presenter/AddTransactionViewPresenter.java
@@ -0,0 +1,65 @@
+package app.presenter;
+
+import app.model.Cargo;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
+public class AddTransactionViewPresenter {
+    @FXML
+    private ChoiceBox<String> contractor;
+    @FXML
+    private TextField purchaseField;
+    @FXML
+    private DatePicker transactionDatePicker;
+    //from address
+    @FXML
+    private TextField fromStreetField;
+    @FXML
+    private TextField fromCityField;
+    @FXML
+    private TextField fromPostalCodeField;
+    @FXML
+    private TextField fromCountryField;
+    //destination address
+    @FXML
+    private TextField destinationStreetField;
+    @FXML
+    private TextField destinationCityField;
+    @FXML
+    private TextField destinationPostalCodeField;
+    @FXML
+    private TextField destinationCountryField;
+   //cargo table
+   @FXML
+   private TableView<Cargo> cargoTable;
+    @FXML
+    private TableColumn<Cargo, String> cargoNameColumn;
+    @FXML
+    private TableColumn<Cargo, String> cargoUnitsColumn;
+    @FXML
+    private TableColumn<Cargo, String> cargoVolumeColumn;
+    @FXML
+    private TableColumn<Cargo, String> cargoWeightColumn;
+    @FXML
+    private Button addCargoButton;
+
+
+
+    @FXML
+    private Button acceptButton;
+    @FXML
+    private Button cancelButton;
+
+    @FXML
+    private void handleAcceptButtonAction(){
+//        TODO
+    }
+    @FXML
+    private void handleCancelButtonAction(){
+//        TODO
+    }
+    @FXML
+    private void handleAddCargoButtonAction(){
+//        TODO
+    }
+}
